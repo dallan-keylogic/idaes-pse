34e03552e3e2ac4cae425cd12251179ffb866350
Doug A <douglas.allan@keylogic.com>
2023-06-15 09:47:04 -0400

    tmp

--- idaes/models/properties/modular_properties/examples/tests/test_entrl_NaBr_mixed_solvent.py
+++ idaes/models/properties/modular_properties/examples/tests/test_entrl_NaBr_mixed_solvent.py
@@ -25,8 +25,9 @@ Author: Andrew Lee
 """
 import pytest
 from math import log
+from copy import deepcopy
 
-from pyomo.environ import ConcreteModel, value
+from pyomo.environ import ConcreteModel, value, units as pyunits
 
 from idaes.models.properties.modular_properties.base.generic_property import (
     GenericParameterBlock,
@@ -34,12 +35,159 @@ from idaes.models.properties.modular_properties.base.generic_property import (
 from idaes.models.properties.modular_properties.examples.enrtl_NaBr_mixed_solvent import (
     configuration,
 )
+from idaes.models.properties.modular_properties.eos.enrtl_reference_states import (
+    InfiniteDilutionAqueous,
+    Unsymmetric,
+    Symmetric
+)
 
+# Data digitized from Fig 5 [1]
+# Form %H2O in solvent by mass, %NaBr in mix by mass
+data = {
+    1.90023164606316: 4.07279483333809,
+    2.52553840508245: 4.60341051665865,
+    3.07268181922432: 5.10150067253983,
+    3.6198252333662: 5.70745038179749,
+    4.3232953372629: 6.59686802660166,
+    5.18309213091442: 7.50180397873668,
+    6.35554230407559: 8.65548586720136,
+    10.420036237701: 11.7202417243546,
+    12.2959565147588: 12.9721116679189,
+    14.0155501020619: 14.2110461502489,
+    15.9696337239972: 15.5348240281863,
+    18.0018806908098: 16.9196471596141,
+    20.1122910024999: 18.3011439277624,
+    21.9658788953071: 19.4438459565312,
+    23.4174838716019: 20.2934710794969,
+    25.0365817297768: 20.9436527661739,
+    26.9906653517121: 21.8041782373378,
+    29.1792390082796: 23.0611962099913,
+    30.7425059058279: 23.7038456814246,
+    32.8529162175179: 24.7168170838022,
+    35.1196532189629: 25.7943190088345,
+    37.230063530653: 26.6777599200545,
+    39.4968005320979: 27.3055144261466,
+    40.9037407398913: 27.8418581569274,
+    42.8578243618266: 28.7814740254471,
+    45.1245613632715: 29.6986205793095,
+    47.2349716749616: 30.2248062217835,
+    53.01905919589: 32.5290639405499,
+    55.2076328524575: 33.3573486018869,
+    57.3180431641476: 33.9680033246278,
+    62.8520079814683: 36.2484067677242,
+    65.0562143070113: 36.6504595612375,
+    66.9929282967516: 37.5059452940427,
+    73.0288754845073: 39.062432020278,
+    75.1392857961974: 39.8922616615831,
+    76.780716038623: 40.8064682302478,
+    83.7372537327126: 42.7720496832428,
+    85.4568473200156: 43.2448153854284,
+    87.1764409073187: 43.3361172522717,
+    88.1144010458476: 43.9206762302395,
+    92.6478750487374: 44.9451625065632,
+    94.3674686360405: 45.3523294496401,
+    96.0870622233435: 45.6278737343614,
+    97.8066558106466: 45.9958029050737,
+}
 
-@pytest.mark.unit
-def model():
+class TestSymmetricNaBr:
+
+    @pytest.fixture(scope="class")
+    def model(self):
+        m = ConcreteModel()
+        m.params = GenericParameterBlock(**configuration)
+
+        m.state = m.params.build_state_block([1])
+
+        # Need to set a value of T for checking expressions later
+        m.state[1].temperature.set_value(298.15)
+
+        # Using 0 results in division by zero errors
+        for k in m.state[1].mole_frac_phase_comp:
+            m.state[1].mole_frac_phase_comp[k].set_value(1e-12)
+
+        return m
+
+    @pytest.mark.unit   
+    def test_solubility(self, model):
+        m = model
+        for x, g in data.items():
+            n_salt = g / 102.9e-3  # MW of NaBr
+            n_H2O = (100 - g) * (x / 100) / 18e-3
+            n_EtOH = (100 - g) * ((100 - x) / 100) / 46e-3
+            n_total = n_H2O + n_EtOH + 2 * n_salt
+
+            m.state[1].mole_frac_phase_comp["Liq", "H2O"].set_value(n_H2O / n_total)
+            m.state[1].mole_frac_phase_comp["Liq", "EtOH"].set_value(n_EtOH / n_total)
+            m.state[1].mole_frac_phase_comp["Liq", "Na+"].set_value(n_salt / n_total)
+            m.state[1].mole_frac_phase_comp["Liq", "Br-"].set_value(n_salt / n_total)
+
+            # Check NaBr solubility product
+            # Note the Ksp given in [1] is actually ln(Ksp)
+            ln_Ksp = value(
+                m.state[1].Liq_log_gamma["Na+"]
+                + log(value(m.state[1].mole_frac_phase_comp["Liq", "Na+"]))
+                + m.state[1].Liq_log_gamma["Br-"]
+                + log(value(m.state[1].mole_frac_phase_comp["Liq", "Br-"]))
+            )
+            assert pytest.approx(-7.157, rel=2e-2) == ln_Ksp
+
+class TestInfiniteDilutionAqueousNaBr:
+    @pytest.fixture(scope="class")
+    def model(self):
+        m = ConcreteModel()
+        config = deepcopy(configuration)
+        config["phases"]["Liq"]["equation_of_state_options"] = {"reference_state": InfiniteDilutionAqueous}
+        # Taken from Table III from
+        # Rashin and Honig, Reevaluation of the Born Model of Ion Hydration, J. Phys. Chem., 1985, 89, 5588-5593
+        config["components"]["Na+"]["parameter_data"] = {"born_radius": (1.680, pyunits.angstrom)}
+        config["components"]["Br-"]["parameter_data"] = {"born_radius": (2.087, pyunits.angstrom)}
+        m.params = GenericParameterBlock(**config)
+
+        m.state = m.params.build_state_block([1])
+
+        # Need to set a value of T for checking expressions later
+        m.state[1].temperature.set_value(298.15)
+
+        # Using 0 results in division by zero errors
+        for k in m.state[1].mole_frac_phase_comp:
+            m.state[1].mole_frac_phase_comp[k].set_value(1e-12)
+
+        return m
+
+    @pytest.mark.unit
+    def test_solubility(self, model):
+        m = model
+        for x, y in data.items():
+            n_salt = y / 102.9e-3  # MW of NaBr
+            n_H2O = (100 - y) * (x / 100) / 18e-3
+            n_EtOH = (100 - y) * ((100 - x) / 100) / 46e-3
+            n_total = n_H2O + n_EtOH + 2 * n_salt
+
+            m.state[1].mole_frac_phase_comp["Liq", "H2O"].set_value(n_H2O / n_total)
+            m.state[1].mole_frac_phase_comp["Liq", "EtOH"].set_value(n_EtOH / n_total)
+            m.state[1].mole_frac_phase_comp["Liq", "Na+"].set_value(n_salt / n_total)
+            m.state[1].mole_frac_phase_comp["Liq", "Br-"].set_value(n_salt / n_total)
+
+            # Check NaBr solubility product
+            # Note the Ksp given in [1] is actually ln(Ksp)
+            ln_Ksp = value(
+                m.state[1].Liq_log_gamma["Na+"]
+                + log(value(m.state[1].mole_frac_phase_comp["Liq", "Na+"]))
+                + m.state[1].Liq_log_gamma["Br-"]
+                + log(value(m.state[1].mole_frac_phase_comp["Liq", "Br-"]))
+            )
+            assert pytest.approx(-7.157, rel=2e-2) == ln_Ksp
+
+if __name__ == "__main__":
     m = ConcreteModel()
-    m.params = GenericParameterBlock(**configuration)
+    config = deepcopy(configuration)
+    config["phases"]["Liq"]["equation_of_state_options"] = {"reference_state": InfiniteDilutionAqueous}
+    # Taken from Table III from
+    # Rashin and Honig, Reevaluation of the Born Model of Ion Hydration, J. Phys. Chem., 1985, 89, 5588-5593
+    config["components"]["Na+"]["parameter_data"] = {"born_radius": (1.680, pyunits.angstrom)}
+    config["components"]["Br-"]["parameter_data"] = {"born_radius": (2.087, pyunits.angstrom)}
+    m.params = GenericParameterBlock(**config)
 
     m.state = m.params.build_state_block([1])
 
@@ -50,59 +198,12 @@ def model():
     for k in m.state[1].mole_frac_phase_comp:
         m.state[1].mole_frac_phase_comp[k].set_value(1e-12)
 
-    # Data digitized from Fig 5 [1]
-    # Form %H2O in solvent by mass, %NaBr in mix by mass
-    data = {
-        1.90023164606316: 4.07279483333809,
-        2.52553840508245: 4.60341051665865,
-        3.07268181922432: 5.10150067253983,
-        3.6198252333662: 5.70745038179749,
-        4.3232953372629: 6.59686802660166,
-        5.18309213091442: 7.50180397873668,
-        6.35554230407559: 8.65548586720136,
-        10.420036237701: 11.7202417243546,
-        12.2959565147588: 12.9721116679189,
-        14.0155501020619: 14.2110461502489,
-        15.9696337239972: 15.5348240281863,
-        18.0018806908098: 16.9196471596141,
-        20.1122910024999: 18.3011439277624,
-        21.9658788953071: 19.4438459565312,
-        23.4174838716019: 20.2934710794969,
-        25.0365817297768: 20.9436527661739,
-        26.9906653517121: 21.8041782373378,
-        29.1792390082796: 23.0611962099913,
-        30.7425059058279: 23.7038456814246,
-        32.8529162175179: 24.7168170838022,
-        35.1196532189629: 25.7943190088345,
-        37.230063530653: 26.6777599200545,
-        39.4968005320979: 27.3055144261466,
-        40.9037407398913: 27.8418581569274,
-        42.8578243618266: 28.7814740254471,
-        45.1245613632715: 29.6986205793095,
-        47.2349716749616: 30.2248062217835,
-        53.01905919589: 32.5290639405499,
-        55.2076328524575: 33.3573486018869,
-        57.3180431641476: 33.9680033246278,
-        62.8520079814683: 36.2484067677242,
-        65.0562143070113: 36.6504595612375,
-        66.9929282967516: 37.5059452940427,
-        73.0288754845073: 39.062432020278,
-        75.1392857961974: 39.8922616615831,
-        76.780716038623: 40.8064682302478,
-        83.7372537327126: 42.7720496832428,
-        85.4568473200156: 43.2448153854284,
-        87.1764409073187: 43.3361172522717,
-        88.1144010458476: 43.9206762302395,
-        92.6478750487374: 44.9451625065632,
-        94.3674686360405: 45.3523294496401,
-        96.0870622233435: 45.6278737343614,
-        97.8066558106466: 45.9958029050737,
-    }
+    log_Ksp_vec = []
 
-    for x, g in data.items():
-        n_salt = g / 102.9e-3  # MW of NaBr
-        n_H2O = (100 - g) * (x / 100) / 18e-3
-        n_EtOH = (100 - g) * ((100 - x) / 100) / 46e-3
+    for x, y in data.items():
+        n_salt = y / 102.9e-3  # MW of NaBr
+        n_H2O = (100 - y) * (x / 100) / 18e-3
+        n_EtOH = (100 - y) * ((100 - x) / 100) / 46e-3
         n_total = n_H2O + n_EtOH + 2 * n_salt
 
         m.state[1].mole_frac_phase_comp["Liq", "H2O"].set_value(n_H2O / n_total)
@@ -112,10 +213,20 @@ def model():
 
         # Check NaBr solubility product
         # Note the Ksp given in [1] is actually ln(Ksp)
-        ln_Ksp = value(
+        log_Ksp_vec.append(value(
             m.state[1].Liq_log_gamma["Na+"]
             + log(value(m.state[1].mole_frac_phase_comp["Liq", "Na+"]))
             + m.state[1].Liq_log_gamma["Br-"]
             + log(value(m.state[1].mole_frac_phase_comp["Liq", "Br-"]))
-        )
-        assert pytest.approx(-7.157, rel=2e-2) == ln_Ksp
+        ))
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+    x_vec = np.array([x for x in data.keys()])
+    log_Ksp_vec = np.array(log_Ksp_vec)
+
+    plt.plot(x_vec, log_Ksp_vec)
+    plt.xlabel("Weight Percent Water")
+    plt.ylabel("log K_sp")
+    plt.title("Inf. Dil. Water")
+    plt.show()
